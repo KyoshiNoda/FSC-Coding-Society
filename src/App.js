361519b75dcf89d0f68bd93b08bfa821069e686e
KyoshiNoda <kyoshisew@gmail.com>
2022-10-17 21:57:51 -0400

    added headless UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Example from './components/Example';
 function App() {
   return (
     <div className='bg-cyan-500'>
       <p className= 'text-3xl text-red-900 text-center'>Tailwind</p>
+      <Example/>
     </div>
   );
 }
