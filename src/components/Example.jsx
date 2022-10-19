f10e7b647f26faeba0b0a41932bfbef33046012e
unknown <kyoshisew@gmail.com>
2022-10-18 21:39:25 -0500

    working on dropdown menu

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -8,7 +8,7 @@ export default function Example() {
     <div className="fixed top-16 w-56 text-right">
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-black hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-black hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring- focus-visible:ring-opacity-75">
             Option
             <ChevronDownIcon
               className="ml-2 -mr-1 h-5 w-5 text-black-200 hover:text-green-100"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import Example from './Example';
 import logo from "../imgs/acmctc-logo.png";
 export default function Navbar({ fixed }) {
+
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav class="flex items-center justify-between flex-wrap bg-green-600 p-4">
+      <nav className="flex items-center justify-between flex-wrap bg-green-600 p-4">
         <a href = "#homepage">
-          <div class="flex items-center flex-shrink-0 text-white hover:text-green-900 mr-6">
+          <div className="flex items-center flex-shrink-0 text-white hover:text-green-900 mr-6">
             <img className="max-h-24 px-4" src={logo} alt="logo"></img>
-            <span class="font-semibold text-5xl tracking-tight">ACM FSC</span>
+            <span className="font-semibold text-5xl tracking-tight">ACM FSC</span>
           </div>
         </a>
 
-        <div class="block lg:hidden">
-          <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <div className="block lg:hidden">
+          <button 
+            className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
             <svg
-              class="fill-current h-3 w-3"
+              className="fill-current h-3 w-3"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -25,25 +28,25 @@ export default function Navbar({ fixed }) {
           </button>
         </div>
 
-        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div class="text-3xl lg:flex-grow"></div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-3xl lg:flex-grow"></div>
           <div>
-            <div class="text-3xl lg:flex-grow">
+            <div className="text-3xl lg:flex-grow"> 
               <a
                 href="#responsive-header"
-                class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-green-900 mr-4 font-bold px-2"
+                className="flex justify-end mt-4 lg:inline-block lg:mt-0 text-white hover:text-green-900 mr-4 font-bold px-2"
               >
                 News
               </a>
               <a
                 href="#responsive-header"
-                class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-green-900 mr-4 font-bold px-2"
+                className="flex justify-end mt-4 lg:inline-block lg:mt-0 text-white hover:text-green-900 mr-4 font-bold px-2"
               >
                 Events
               </a>
               <a
                 href="#responsive-header"
-                class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-green-900 font-bold px-2"
+                className="flex justify-end mt-4 lg:inline-block lg:mt-0 text-white hover:text-green-900 font-bold px-2"
               >
                 Executives
               </a>
