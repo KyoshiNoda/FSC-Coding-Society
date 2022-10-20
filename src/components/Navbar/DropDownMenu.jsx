678c9f7154f5cfa08e69b6e3dac7d93d1765445e
unknown <kyoshisew@gmail.com>
2022-10-19 19:21:20 -0500

    responsive with dropdown

--- src/components/Navbar/DropDownMenu.jsx
+++ src/components/Navbar/DropDownMenu.jsx
@@ -5,7 +5,7 @@ import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 const DropDownMenu = () => {
   return (
-    <div className="lg:hidden top-16 w-56 text-right">
+    <div className="lg:hidden top-16 w-auto text-right">
       <Menu as="div" className="relative inline-block text-left">
         <div>
           <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-black hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring- focus-visible:ring-opacity-75">
@@ -25,7 +25,7 @@ const DropDownMenu = () => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="absolute right-0 mt-2 w-auto origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="px-1 py-1 ">
               <Menu.Item>
                 {({ active }) => (
