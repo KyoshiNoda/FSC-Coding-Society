3fd6a873242836c65256259d306aef7123e23ad4
KyoshiNoda <kyoshisew@gmail.com>
2022-11-02 11:43:37 -0400

    created mainEvent component

---
+++ src/components/Events/MainEvent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function MainEvent() {
+  return (
+    <div className="flex flex-col h-96 text-white bg-green-900">
+      <div className="flex justify-center text-2xl">Main Event Title</div>
+      
+      <div className="">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        officia incidunt ipsam fugiat totam quasi, commodi nesciunt suscipit
+        omnis corrupti assumenda, at maxime nostrum neque fuga labore aut iure.
+        Fugit. dolorem?
+        <div className="flex justify-center">
+          <u>Learn more</u>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainEvent;
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,61 +1,50 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
-import SearchBar from '../components/Events/SearchBar/SearchBar';
+import SearchBar from "../components/Events/SearchBar/SearchBar";
+import MainEvent from "../components/Events/MainEvent";
 function Events() {
   return (
-    <div className="">
+    <>
       <Navbar />
       <SearchBar />
-
-      <div className="flex-col">
-
-        <div className="bg-green-800 justify-center h-96 text-white">
-          <div className="flex justify-center"> Main Event Title</div>
-          <div className=" items-end">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore aut
-            hic doloribus laborum quam ad minus, eveniet velit maiores nostrum
-            expedita autem, corrupti earum blanditiis! Cum incidunt voluptate
-            modi iure. Cum veritatis rem, nesciunt explicabo nam tempore vitae
-            aperiam architecto officia tenetur pariatur aliquam sunt dolores.
-            Expedita animi quisquam quam ex recusandae voluptate asperiores hic
-            tenetur, sapiente tempora et dolorem?
-            <div className="flex justify-center"> <u>Learn more</u></div>
-          </div>
-        </div>
-      </div>
-
+      <MainEvent/>
+      
       <div className="flex">
-        <div>
-          <div>Title</div>
+        <div className="bg-red-600 ">
+          <div className="flex justify-center text-2xl">
+            Title
+          </div>
           <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odit illum, autem ipsam ut facere nisi est numquam, culpa itaque neque doloremque ullam delectus eveniet recusandae porro explicabo amet. Perspiciatis!
-            Dicta ipsum facere illum, tenetur officia culpa odio neque sed et delectus autem, ea maxime a asperiores eius recusandae, incidunt quisquam excepturi quasi temporibus assumenda? Sunt minima dolore itaque eaque!
-            <div className= "flex justify-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            odit illum, autem ipsam ut facere nisi est numquam, culpa itaque
+            neque doloremque ullam delectus eveniet recusandae porro explicabo
+            amet. Perspiciatis! Dicta ipsum facere illum, tenetur officia culpa
+            odio neque sed et delectus autem, ea maxime a asperiores eius
+            recusandae, incidunt quisquam excepturi quasi temporibus assumenda?
+            Sunt minima dolore itaque eaque!
+            <div className="flex justify-center">
               <u>Learn More</u>
             </div>
           </div>
         </div>
+
         <div>
-          <div>Title</div>
+          <div className="flex justify-center">Title</div>
           <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odit illum, autem ipsam ut facere nisi est numquam, culpa itaque neque doloremque ullam delectus eveniet recusandae porro explicabo amet. Perspiciatis!
-            Dicta ipsum facere illum, tenetur officia culpa odio neque sed et delectus autem, ea maxime a asperiores eius recusandae, incidunt quisquam excepturi quasi temporibus assumenda? Sunt minima dolore itaque eaque!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            odit illum, autem ipsam ut facere nisi est numquam, culpa itaque
+            neque doloremque ullam delectus eveniet recusandae porro explicabo
+            amet. Perspiciatis! Dicta ipsum facere illum, tenetur officia culpa
+            odio neque sed et delectus autem, ea maxime a asperiores eius
+            recusandae, incidunt quisquam excepturi quasi temporibus assumenda?
+            Sunt minima dolore itaque eaque!
             <div className="flex justify-center">
               <u>Learn More</u>
             </div>
           </div>
         </div>
       </div>
-
-      
-
-      
-
-
-
-    </div>
-
-    
+    </>
   );
 }
 
