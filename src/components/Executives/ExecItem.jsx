771924530b2cd86a9a8cece58dc3f741f0b65710
KyoshiNoda <kyoshisew@gmail.com>
2022-11-03 19:01:10 -0400

    added structure and role title

--- src/components/Executives/ExecItem.jsx
+++ src/components/Executives/ExecItem.jsx
@@ -5,7 +5,8 @@ function ExecItem() {
     <div class="bg-green-500 max-w-sm rounded overflow-hidden shadow-lg">
       <img class="w-full" src={logo} alt="Sunset in the mountains" />
       <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">John Doe</div>
+        <div class="font-bold text-2xl mb-2">John Doe</div>
+        <div class = "text-white font-bold text-xl mb-2">ROLE</div>
         <p class="text-gray-700 text-base">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
           quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
--- src/components/Executives/ExecList.jsx
+++ src/components/Executives/ExecList.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ExecItem from './ExecItem'
+import React from "react";
+import ExecItem from "./ExecItem";
 function ExecList() {
   return (
-    <div>
-        <ExecItem/>
-        <ExecItem/>
-        <ExecItem/>
-        <ExecItem/>
+    <div className="flex flex-col space-y-11">
+      <div className="flex">
+        <ExecItem />
+        <ExecItem />
+      </div>
+      <div className="flex">
+        <ExecItem />
+        <ExecItem />
+      </div>
     </div>
-  )
+  );
 }
 
-export default ExecList
+export default ExecList;
