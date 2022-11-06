f0a0fffb82d8df153a2d07105fa6756dc30a6ef9
unknown <kyoshisew@gmail.com>
2022-11-06 15:19:42 -0500

    react router bug fixing for github pages

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {HashRouter} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
