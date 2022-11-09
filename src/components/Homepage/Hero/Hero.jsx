1e1637a424eb884edcddedc9a457938766b4fb3a
unknown <kyoshisew@gmail.com>
2022-11-09 01:14:27 -0500

    added ram central link to hero button

--- src/components/Homepage/Hero/Hero.jsx
+++ src/components/Homepage/Hero/Hero.jsx
@@ -10,9 +10,11 @@ function Hero() {
                 Share Ideas.
             </div>
             <div className='flex justify-center'>
+                <a href = "https://farmingdale.campuslabs.com/engage/organization/computer-tech-club">
                 <button class="w-48 h-14 bg-gradient-to-r from-green-700 via-green-800 to-green-900 text-white font-bold py-2 px-4 rounded-full">
                     Get Involved!
                 </button>
+                </a>
                 
             </div>
         </div>
