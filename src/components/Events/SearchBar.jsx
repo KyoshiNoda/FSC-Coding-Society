ddbafae921c29118bc7975dc9ff1758a05ae4296
unknown <kyoshisew@gmail.com>
2022-11-11 23:40:52 -0500

    fixed console errors

--- src/components/Events/SearchBar.jsx
+++ src/components/Events/SearchBar.jsx
@@ -6,24 +6,24 @@ function SearchBar() {
       <form>
         <label
           htmlFor="default-search"
-          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+          className ="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
         >
           Search
         </label>
-        <div class="relative">
-          <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+        <div className ="relative">
+          <div className ="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
             <svg
               aria-hidden="true"
-              class="w-5 h-5 text-gray-500 dark:text-gray-400"
+              className ="w-5 h-5 text-gray-500 dark:text-gray-400"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               ></path>
             </svg>
@@ -31,13 +31,13 @@ function SearchBar() {
           <input
             type="search"
             id="default-search"
-            class="block p-4 pl-10 w-full text-sm text-black rounded-lg border border-gray-30"
+            className ="block p-4 pl-10 w-full text-sm text-black rounded-lg border border-gray-30"
             placeholder="Search..."
             required=""
           />
           <button
             type="submit"
-            class="text-white absolute right-2.5 bottom-2.5 bg-green-700 hover:bg-green-900 focus:ring-4 font-medium rounded-lg text-sm px-4 py-2"
+            className  ="text-white absolute right-2.5 bottom-2.5 bg-green-700 hover:bg-green-900 focus:ring-4 font-medium rounded-lg text-sm px-4 py-2"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
